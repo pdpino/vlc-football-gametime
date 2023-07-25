aab9bfa9a17c5016ad3c204f73126027f66b3af0
Pablo Pino <pdpinog@gmail.com>
2023-07-25 00:30:23 -0400

    Update setup script for linux

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+## Install in linux
+
+# Copy necessary files
 cp football_gametime_extension.lua $HOME/.local/share/vlc/lua/extensions/football.lua
 cp football_gametime_interface.lua $HOME/.local/share/vlc/lua/intf/football.lua
 
-# in bash_aliases:
-# alias vlc-football="vlc --verbose=1 --extraintf=luaintf --lua-intf=football"
+# Add line to bash_aliases
+
+if [[ ! -f ~/.bash_aliases ]] || [[ -z $(egrep "^alias vlc-football.*football" ~/.bash_aliases) ]]; then
+  echo "alias vlc-football='vlc --verbose=1 --extraintf=luaintf --lua-intf=football'" >> ~/.bash_aliases
+fi
